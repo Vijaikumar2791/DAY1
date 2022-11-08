32f4887b1da6f5029b34248cb80b6f5f659b6561
Vijaikumar2791 <v.vijaikumar2791@gmail.com>
2022-11-08 13:56:19 +0530

    Project first add

---
+++ src/test/java/org/project1st/text1.java
@@ -0,0 +1,15 @@
+package org.project1st;
+
+import org.testng.annotations.Test;
+
+public class text1 {
+
+	@Test
+	private void test() {
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+		
+		
+	}
+}
---
+++ src/test/java/org/project1st/text3.java
@@ -0,0 +1,5 @@
+package org.project1st;
+
+public class text3 {
+
+}
